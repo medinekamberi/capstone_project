f1e27c2098aab1c6a346d6402393b600144bd52d
medina <medine.kamberi@gmail.com>
2019-05-08 00:07:10 +0200

    All crud operations work

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, jsonify,render_template, flash , redirect, url_for, session, logging
 from flask_sqlalchemy import SQLAlchemy 
 from flask_marshmallow import Marshmallow #is an ORM/ODM/framework-agnostic library for converting complex datatypes, such as objects, to and from native Python datatypes.
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
-from passlib.hash import sha256_crypt
-from functools import wraps
+from werkzeug.security import generate_password_hash
+# from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+# from passlib.hash import sha256_crypt
+# from functools import wraps
 
 import os #python module deals with file path
 import datetime
@@ -23,7 +24,7 @@ marshmallow= Marshmallow(app)
 class User(db.Model):
     id= db.Column(db.Integer(),primary_key=True, autoincrement=True)
     name = db.Column(db.String(64))
-    username=db.Column(db.String(64))
+    surname=db.Column(db.String(64))
     email=db.Column(db.String(64))
     password=db.Column(db.String(256))
     role= db.Column(db.String(64))
@@ -31,11 +32,11 @@ class User(db.Model):
     created_at=db.Column(db.DateTime)
     updated_at=db.Column(db.DateTime)
     accounts=db.relationship('Account', backref='user', lazy=True)
-    def __init__(self,name,username,email,password,role,active,created_at,updated_at, accounts):
+    def __init__(self,name,surname,email,password,role,active,created_at,updated_at, accounts):
         self.name=name
-        self.username=username
+        self.surname=surname
         self.email=email
-        self.password=password
+        self.password=generate_password_hash(password)
         self.role=role
         self.active=active
         self.created_at=created_at
@@ -44,7 +45,7 @@ class User(db.Model):
 #User schema
 class UserSchema(marshmallow.Schema):
     class Meta:
-        fields=('name','username','email','password','role','active','created_at','updated_at', 'accounts')
+        fields=('name','surname','email','password','role','active','created_at','updated_at', 'accounts')
 #Init schema 
 user_schema=UserSchema(strict=True)
 users_schema = UserSchema(many=True, strict=True)
@@ -57,18 +58,17 @@ class Account(db.Model):
     updated_at=db.Column(db.DateTime)
     transactions=db.relationship('Transaction', backref='account', lazy=True)
     user_id=db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    def __init__(self,balance,currency,created_at,updated_at, transactions, user_id):
+    def __init__(self,balance,currency,created_at,updated_at, user_id):
         self.balance=balance
         self.currency=currency
         self.created_at=created_at
         self.updated_at=updated_at
-        self.transactions=transactions
         self.user_id=user_id
 
 #Account Schema
 class AccountSchema(marshmallow.Schema):
     class Meta:
-        fields=('id','balance','currency','created_at','updated_at', 'transactions', 'user_id')
+        fields=('id','balance','currency','created_at','updated_at', 'user_id')
 
 #init account schema
 account_schema=AccountSchema(strict=True)
@@ -117,58 +117,34 @@ transactions_schema= TransactionSchema(many=True, strict=True)
 def index():
     return render_template('index.html')
 #Register Form Class
-class RegisterForm(Form):
-    name = StringField('Name', [validators.Length(min=1, max=50)])
-    username = StringField('Username', [validators.Length(min=4, max=25)])
-    email = StringField('Email', [validators.Length(min=6, max=50)])
-    password = PasswordField('Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords do not match')
-    ])
-    confirm = PasswordField('Confirm Password')
+# class RegisterForm(Form):
+#     name = StringField('Name', [validators.Length(min=1, max=50)])
+#     surname = StringField('surname', [validators.Length(min=4, max=25)])
+#     email = StringField('Email', [validators.Length(min=6, max=50)])
+#     password = PasswordField('Password', [
+#         validators.DataRequired(),
+#         validators.EqualTo('confirm', message='Passwords do not match')
+#     ])
+#     confirm = PasswordField('Confirm Password')
 
 #Create a user
 @app.route('/signup',methods=['GET','POST'])
 def create_user():
-    form= RegisterForm(request.form)
-    if request.method == 'POST' and form.validate():
-        user = User(form.name.data, form.username.data, form.email.data,form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('Thanks for registering')
-        return redirect(url_for('login'))
-    return render_template('signup.html', form=form)
-    #     name= form.name.data
-    #     email=form.email.data
-    #     username=form.username.data
-    #     password = sha256_crypt.encrypt(str(form.password.data))
-
-    #     #create cursor
-    #     cur = db.connection.cursor()
-    #     #execute query
-    #     cur.execute("INSERT INTO users(name, email, username, password)VALUES(%s,%s,%s)",(name, email,username,password))
-    #     ##commit to db
-    #     db.session.commit()
-    #     cur.close()
-    #     flash('You are now registered and can log in', 'success')
-    #     return redirect(url_for('index.html'))
-    # return render_template('signup.html', form=form)
+    name=request.json['name']
+    surname=request.json['surname']
+    email=request.json['email']
+    password=request.json['password']
+    role=request.json['role']
+    active=request.json['active']
+    created_at=datetime.datetime.now()
+    updated_at=datetime.datetime.now()
+    accounts=request.json['accounts']
+    new_user= User(name,surname,email,password,role,active,created_at,updated_at, accounts)
     
-    # name=request.json['name']
-    # username=request.json['username']
-    # email=request.json['email']
-    # password=request.json['password']
-    # role=request.json['role']
-    # active=request.json['active']
-    # created_at=datetime.datetime.now()
-    # updated_at=datetime.datetime.now()
-    # accounts=request.json['accounts']
-    # new_user= User(name,username,email,password,role,active,created_at,updated_at, accounts)
-    
-    # db.session.add(new_user)
-    # db.session.commit()
+    db.session.add(new_user)
+    db.session.commit()
 
-    # return user_schema.jsonify(new_user)
+    return user_schema.jsonify(new_user)  
 #Get all users
 @app.route('/signup', methods=['GET'])
 def get_users():
@@ -190,23 +166,21 @@ def delete_user(id):
     db.session.commit()
     
     return user_schema.jsonify(user)
-#Update a user, it is not working
+#Update a user
 @app.route('/signup/<id>', methods=['PUT'])
 def update_user(id):
     user = User.query.get(id)
-
+    print(user)
     name=request.json['name']
-    username=request.json['username']
+    surname=request.json['surname']
     email=request.json['email']
     password=request.json['password']
     role=request.json['role']
     active=request.json['active']
-    created_at=datetime.datetime.now()
-    updated_at=datetime.datetime.now()
     accounts=request.json['accounts']
     
     user.name=name
-    user.username=username
+    user.surname=surname
     user.email=email
     user.password=password
     user.role=role
@@ -217,23 +191,23 @@ def update_user(id):
 
     return user_schema.jsonify(user)
 
-#Create an account, it is not working
+#Create an account
 @app.route('/account', methods=['POST'])
 def create_account():
-    balance=request.json['balance']
+    balance = request.json['balance']
     currency=request.json['currency']
     created_at=datetime.datetime.now()
     updated_at=datetime.datetime.now()
-    transactions=request.json['transactions']
     user_id=request.json['user_id']
     
-    new_account=Account(balance,currency, created_at, updated_at,transactions,user_id)
+    new_account=Account(balance,currency, created_at, updated_at,user_id)
     
     db.session.add(new_account)
     db.session.commit()
     
-    return user_schema.jsonify(new_account)
-#Get all accounts, not tested
+    return account_schema.jsonify(new_account)
+
+#Get all accounts
 @app.route('/account', methods=['GET'])
 def get_accounts():
     all_accounts = Account.query.all()
@@ -241,34 +215,30 @@ def get_accounts():
     
     return jsonify(result.data)
 
-#Get account by id, not tested
-@app.route('/account<id>', methods=['GET'])
+#Get account by id
+@app.route('/account/<id>', methods=['GET'])
 def get_account(id):
     account = Account.query.get(id)
     
     return account_schema.jsonify(account)
-#Update an account, not tested
-@app.route('/account<id>', methods=['PUT'])
+#Update an account
+@app.route('/account/<id>', methods=['PUT'])
 def update_account(id):
     account = Account.query.get(id)
 
     balance= request.json['balance']
     currency = request.json['currency']
-    created_at=datetime.datetime.now()
-    updated_at=datetime.datetime.now()
-    transactions= request.json['transactions']
     user_id=request.json['user_id']
 
     account.balance=balance
     account.currency=currency
-    account.transactios=transactions
     account.user_id=user_id
 
     db.session.commit()
 
     return account_schema.jsonify(account)
-#Delete an account, not tested
-@app.route('/account<id>', methods=['DELETE'])
+#Delete an account
+@app.route('/account/<id>', methods=['DELETE'])
 def delete_account(id):
     account = Account.query.get(id)
     db.session.delete(account)
@@ -277,7 +247,7 @@ def delete_account(id):
     return account_schema.jsonify(account)
 
 
-#Create a transaction, not tested
+#Create a transaction, amount is always 0!!
 @app.route('/transaction', methods=['POST'])
 def create_transaction():
     title= request.json['title']
@@ -285,24 +255,39 @@ def create_transaction():
     transaction_type= request.json['transaction_type']
     created_at=datetime.datetime.now()
     updated_at=datetime.datetime.now()
+    account_id = request.json['account_id']
     
-    new_account= Account(title,amount,transaction_type,created_at,updated_at,user_id)
+    new_transaction= Transaction(title,amount,transaction_type,created_at,updated_at,account_id)
 
-    db.session.add(new_account)
+    db.session.add(new_transaction)
     db.session.commit()
 
-    return transaction_schema.jsonify(new_account)      
+    return transaction_schema.jsonify(new_transaction)      
+#Update a transaction 
+@app.route('/transaction/<id>', methods=['PUT'])
+def update_transaction(id):
+    transaction = Transaction.query.get(id)
+    title = request.json['title']
+    amount = request.json['amount']
+    transaction_type = request.json['transaction_type']
+
+    transaction.transaction=transaction
+    transaction.title=title
+    transaction.amount = amount
+    transaction.transaction_type= transaction_type
+
+    db.session.commit()
+
+    return transaction_schema.jsonify(transaction)
     
-    #account_id=db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
-    
-#Get all transactions, not tested
+#Get all transactions
 @app.route('/transaction', methods=['GET'])
 def get_transactions():
     all_transactions = Transaction.query.all()
     result = transactions_schema.dump(all_transactions)
     return jsonify(result.data)
 
-#Get single transaction, not tested
+#Get single transaction
 @app.route('/transaction/<id>', methods=['GET'])
 def get_transaction(id):
     transaction = Transaction.query.get(id)
